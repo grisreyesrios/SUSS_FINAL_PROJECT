d437bf64beef05053890d763314cb16aae4aac37
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-28 06:02:39 +0100

    Adding searching bar with form

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -5,11 +5,7 @@ class Workshop < ApplicationRecord
   validates :price, numericality: { only_integer: true }
 
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings, dependent: :destroy
+  # has_many :reviews, through: :bookings, dependent: :destroy
 
-<<<<<<< HEAD
-  belongs_to :user, optional:true
-=======
   belongs_to :user, optional: true
->>>>>>> e37138ea184d984dd7889fd8aa3314442af70981
 end
