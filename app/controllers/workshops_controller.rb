518c4790663a5c52b777f94386d831b2ac64e29d
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-28 04:35:53 +0100

    Changes to workshops controller to solve problem with user

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,13 +1,9 @@
 class WorkshopsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
-<<<<<<< HEAD
-  before_action :find_and_authorize_current_workshop, only: [:show, :edit, :update, :destroy]
-=======
   before_action :find_and_authorize_current_workshop, only: [:show, :edit, :update, :destroy, :new]
->>>>>>> c280e879268e4bdcf5e93df88ba23bf3ff534959
 
   def index
-    # @workshops = Workshop.all
+    @workshops = Workshop.all
     @workshops = policy_scope(Workshop).order(created_at: :desc)
   end
 
@@ -16,10 +12,11 @@ class WorkshopsController < ApplicationController
     @workshop = Workshop.new
   end
 
+
   private
 
   def find_and_authorize_current_workshop
-    @workshop = Workshop.find(params[:id])
+    @workshop = Workshop.find(params[:user_id])
     authorize @workshop
   end
 end
