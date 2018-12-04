b22e531f9ad8e7d0463ccc10f0b8cf3b9af37e91
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-12-04 15:35:26 +0100

    Adding draggable search

--- app/javascript/plugins/draggable.js
+++ app/javascript/plugins/draggable.js
@@ -1,15 +1,35 @@
-import { Droppable } from '@shopify/draggable';
-
-const droppable = new Droppable(document.querySelectorAll('.droppable-container'), {
-  draggable: '.draggable-item',
-  dropzone: '.droppable-dropzone'
-});
-
-console.log(droppable)
-droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
-droppable.on('droppable:returned', () => console.log('droppable:returned'));
+import { Sortable } from '@shopify/draggable';
 
 
+function sortItems() {
+  const sortable = new Sortable(document.querySelectorAll('.sortable-dropzone'), {
+    draggable: '.sortable-item'
+  });
+
+  const form = document.getElementById('searchForm');
+  sortable.on('sortable:stop', () => {
+    const dropzone = document.getElementById('search-box-drop');
+    const items = Array.from(dropzone.children);
+    removeAddedField()
+    items.forEach((item, index) => {
+      const value = item.dataset.value;
+      const categoryField = document.getElementById('categories');
+      const cloneField = categoryField.cloneNode(true)
+      cloneField.id = categoryField.id + "_" + index
+      cloneField.value = value
+      cloneField.classList.add('category-field-added')
+      console.log(cloneField.value)
+      categoryField.insertAdjacentElement('afterend',cloneField);
+    })
+  });
+}
+
+sortItems();
+
+function removeAddedField() {
+  const fields = document.querySelectorAll('#searchForm > .category-field-added')
+  fields.forEach((field) => field.remove())
+}
 /*setInterval( function search()  {
   var count = 0
   var searchBox = document.querySelector("#search-box-drop > ul")
